6a7bd8211cbdff00fbf04bfa0311c76cfaf704f6
Jack Kim <jack@jackkim.dev>
2021-10-15 12:28:44 -0700

    Add routing and history; Fix material-ui deprecation bugs

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,27 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
+    // this is a copy of the Browser History used by Container
+    const history = useHistory();
 
     useEffect(() => {
         // ref.current is the reference to the HTML element we want to mount MarketingApp to
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+
+                // Check to prevent infinite loop
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App'
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    // If we were provided a defaultHistory (dev env) use it, otherwise use memory history in prod
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        // .listen is a event listener tied to history
+        // whenever a nav event occurs, this will call any function that we passed in
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    // Functions that Container can call to update/change Marketing
+    return {
+        // { pathname } is passed in by history.listen()
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            // Check to prevent infinite loop
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 }
 
 // If we are in development and in isolation,
@@ -16,7 +39,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
